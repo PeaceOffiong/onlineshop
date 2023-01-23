4f5aa7e3c44ad1a60cab2a784793cf0c3e691daf
peaceOffiong <peaceyben@gamil.com>
2023-01-22 20:21:31 -0800

    backend programmed

--- Backend/products.js
+++ Backend/products.js
@@ -1,46 +1,52 @@
 const products = [
-    {
-        id: 1,
-        name: "iphone 12 Pro",
-        desc: "6.1 inch display",
-        price: 999,
-        image: ""
-    },
-    {
-        id: 2,
-        name: "iphone 12",
-        desc: "5 4 inch display",
-        price: 699,
-        image: ""
-    },
-    {
-        id: 3,
-        name: "Galaxy S",
-        desc: "6.5 inch display",
-        price: 399,
-        image: ""
-    },
-    {
-        id: 4,
-        name: "iphone xr",
-        desc: "5.1 inch display",
-        price: 879,
-        image: ""
-    },
-    {
-        id: 5,
-        name: "Nokia",
-        desc: "4.1 inch display",
-        price: 299,
-        image: ""
-    },
-    {
-        id: 6,
-        name: "iphone 12 Pro",
-        desc: "6.1 inch display",
-        price: 999,
-        image: ""
-    }
-]
+  {
+    id: 1,
+    name: "iphone 12 Pro",
+    desc: "6.1 inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290259/online%20shop/iphone12pro_ekws8r.jpg",
+  },
+  {
+    id: 2,
+    name: "iphone 12",
+    desc: "5 4 inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290281/online%20shop/galaxyS_c1zf1d.png",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5 inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290281/online%20shop/galaxyS_c1zf1d.png",
+  },
+  {
+    id: 4,
+    name: "iphone xr",
+    desc: "5.1 inch display",
+    price: 879,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290883/online%20shop/download_quol5t.jpg",
+  },
+  {
+    id: 5,
+    name: "Nokia",
+    desc: "4.1 inch display",
+    price: 299,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290829/online%20shop/download_bwunga.jpg",
+  },
+  {
+    id: 6,
+    name: "iphone 12 Pro",
+    desc: "6.1 inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/du8oaagwi/image/upload/v1674290281/online%20shop/galaxyS_c1zf1d.png",
+  },
+];
 
 module.exports = products;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import productReducer, { productsFetch } from "./features/productSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productReducer,
+  },
+});
+
+store.dispatch(productsFetch());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
